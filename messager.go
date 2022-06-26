599ae904f79c52f7c39351f02c9d9fbf8ff3f4bd
liulian <liulian@gaoding.com>
2022-06-26 14:57:20 +0800

    打印body

--- messager.go
+++ messager.go
@@ -15,7 +15,8 @@ func main() {
 }
 
 func echoAll(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // 解析参数，默认是不会解析的
+	r.ParseForm() // 解析参数，默认是不会解析的
+	fmt.Println(r.Body)
 	fmt.Println(r.Form) // 这些信息是输出到服务器端的打印信息
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
