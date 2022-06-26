41620c4ba7c3a3dd919e36e8b1b73241c8153068
liulian <liulian@gaoding.com>
2022-06-26 15:05:58 +0800

    fix

--- messager.go
+++ messager.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -16,7 +17,8 @@ func main() {
 
 func echoAll(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm() // 解析参数，默认是不会解析的
-	fmt.Println(r.Body)
+	s, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(s)
 	fmt.Println(r.Form) // 这些信息是输出到服务器端的打印信息
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
